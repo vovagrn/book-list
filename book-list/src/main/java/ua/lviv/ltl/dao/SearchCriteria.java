e74e4b460f2c34316f01d536ab573066f8b5bd7f
Vavan <Vavan@192.168.1.104>
2018-03-21 08:47:43 +0200

    updae

--- book-list/src/main/java/ua/lviv/ltl/dao/SearchCriteria.java
+++ book-list/src/main/java/ua/lviv/ltl/dao/SearchCriteria.java
@@ -1,35 +1,52 @@
 package ua.lviv.ltl.dao;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-public class SearchCriteria<E extends Enum<E>>{
-	
-	public enum SortType{
+import ua.lviv.ltl.dao.impl.SearchEntry;
+
+public class SearchCriteria<E extends Enum<E>> {
+
+	public enum SortType {
 		ASC, DESC
 	}
-	
+
+	public SearchCriteria() {
+		super();
+	}
+
+	public SearchCriteria(int start, int pageSize) {
+		super();
+		this.pagination = true;
+		this.start = start;
+		this.pageSize = pageSize;
+	}
+
 	private boolean pagination = false;
+	private int start = 0;
 	private int pageSize = 0;
-	private Map<E, SortType> searchTypes;
+	private List<SearchEntry> searcEntrys = new ArrayList();
 	
-	public Map<E, SortType> getSearchTypes() {
-		return searchTypes;
+	public SearchCriteria<E> addSearchEntry(SearchEntry searchEntry){
+		searcEntrys.add(searchEntry);
+		return this;
 	}
-	public void setSearchTypes(Map<E, SortType> searchTypes) {
-		this.searchTypes = searchTypes;
-	}
+
 	public boolean isPagination() {
 		return pagination;
 	}
+
 	public void setPagination(boolean pagination) {
 		this.pagination = pagination;
 	}
+
 	public int getPageSize() {
 		return pageSize;
 	}
+
 	public void setPageSize(int pageSize) {
 		this.pageSize = pageSize;
 	}
-	
 
 }
---
+++ book-list/src/main/java/ua/lviv/ltl/dao/impl/SearchEntry.java
@@ -0,0 +1,5 @@
+package ua.lviv.ltl.dao.impl;
+
+public class SearchEntry {
+
+}
